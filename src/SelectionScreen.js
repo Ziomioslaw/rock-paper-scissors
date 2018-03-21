007aaff7211903d57435edf37cdc915712adda03
Ziomioslaw <ziomioslaw@wp.pl>
2018-03-21 21:27:40 +0100

    Base draft of GUI

---
+++ src/SelectionScreen.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class SelectionScreen extends Component {
+  render() {
+    return (
+      <p>Selection Screen</p>
+    )
+  }
+}
+
+export default SelectionScreen
