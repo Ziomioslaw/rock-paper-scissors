fd590c1ab33a616a755750af3a58aa55b7f6fc1e
Ziomioslaw <ziomioslaw@wp.pl>
2018-03-22 20:19:03 +0100

    Adding drafts buttons

--- src/SelectionScreen.js
+++ src/SelectionScreen.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
 
 class SelectionScreen extends Component {
+
+  onButtonClick(type: string) {
+    console.log(this, type)
+  }
+
   render() {
     return (
-      <p>Selection Screen</p>
+      <div>
+        <div onClick={this.onButtonClick.bind(this, 'rock')} className="rock">Rock</div>
+        <div onClick={this.onButtonClick.bind(this, 'paper')} className="paper">Paper</div>
+        <div onClick={this.onButtonClick.bind(this, 'scissors')} className="scissors">Scissors</div>
+      </div>
     )
   }
 }
