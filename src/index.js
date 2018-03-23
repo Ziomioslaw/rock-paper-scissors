248eb62700e007ff18581527d8e72bb29144f135
Ziomioslaw <ziomioslaw@wp.pl>
2018-03-23 22:22:48 +0100

    Instalating Redux library

--- src/index.js
+++ src/index.js
@@ -4,5 +4,24 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { connect } from 'react-redux'
+import { createStore } from 'redux'
+
+const reducer = (state, action) => {
+  console.log(state, action)
+
+  switch (action) {
+    case 'USER_CHOSE_ROCK':
+      return state
+    case 'USER_CHOSE_PAPER':
+      return state
+    case 'USER_CHOSE_SCISSORS':
+      return state
+  }
+}
+
+const initialState = {count: 0}
+const store = createStore(reducer, initialState)
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
