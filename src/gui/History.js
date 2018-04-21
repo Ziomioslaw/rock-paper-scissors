322c269ec0ffe2dc435bf00c7353eed227027ff1
Ziomioslaw <ziomioslaw@wp.pl>
2018-04-21 16:37:09 +0200

    Nicer display of battle result

--- src/gui/History.js
+++ src/gui/History.js
@@ -3,11 +3,21 @@ import { connect } from 'react-redux'
 import './History.css'
 
 
+function winArrayToString(array) {
+  if (array[0] !== array[1]) {
+    return array[0] > array[1]
+      ? 'AI win'
+      : 'Player win'
+  }
+
+  return 'Draw'
+}
+
 class History extends Component {
   render() {
     const history = this.props.history.map((item, index) => (
       <p key={index}>
-        {item[0]} {item[1]} {item[2]}
+        {item[0]} {item[1]} [{winArrayToString(item[2])}]
       </p>
     ))
 
