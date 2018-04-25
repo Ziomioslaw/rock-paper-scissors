57dbb6027f4e6b32826056a3aaf482517183e943
Ziomioslaw <ziomioslaw@wp.pl>
2018-04-25 22:18:43 +0200

    Adding graphics for buttons

--- src/gui/SelectionScreen.js
+++ src/gui/SelectionScreen.js
@@ -37,9 +37,9 @@ class SelectionScreen extends Component {
       <section>
         <header>Choose your sign:</header>
         <section>
-          <div className="sign rock" onClick={this.onButtonClick.bind(this, 'rock')}>Rock</div>
-          <div className="sign paper" onClick={this.onButtonClick.bind(this, 'paper')}>Paper</div>
-          <div className="sign scissors" onClick={this.onButtonClick.bind(this, 'scissors')}>Scissors</div>
+          <div className="sign rock" onClick={this.onButtonClick.bind(this, 'rock')}></div>
+          <div className="sign paper" onClick={this.onButtonClick.bind(this, 'paper')}></div>
+          <div className="sign scissors" onClick={this.onButtonClick.bind(this, 'scissors')}></div>
         </section>
       </section>
     )
