1c5f5f03aca8d93236d6b05190d62065ab6f2a16
Ziomioslaw <ziomioslaw@wp.pl>
2018-05-16 17:26:22 +0200

    Displaying the percantege results

--- src/gui/PointsDisplay.js
+++ src/gui/PointsDisplay.js
@@ -5,18 +5,27 @@ import './PointsDisplay.css'
 
 class PointsDisplay extends Component {
   render() {
+    console.log('x', this.props  )
+    const percentage_span = this.props.all_plays
+      ? (this.props.player_points / this.props.all_plays).toFixed(2) + '%'
+      : '-'
+
     return <header>
       <span>AI: {this.props.ai_points}</span>
       <span>You: {this.props.player_points}</span>
+      <span>{percentage_span}</span>
     </header>
   }
 }
 
-
 const mapStateToProps = store => {
+  console.log(store.history)
   return {
     ai_points: store.ai_points,
-    player_points: store.player_points
+    player_points: store.player_points,
+    all_plays: (store.history || false)
+      ? store.history.length
+      : 0
   }
 }
 
